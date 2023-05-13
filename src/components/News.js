1157e68bc43a514fc3565e1ed46bd900a2aed8e9
tahany777 <tahanymohammed777@hotmail.com>
2023-05-13 16:14:01 +0300

    add delete and get news

--- src/components/News.js
+++ src/components/News.js
@@ -1,7 +1,53 @@
-import React from "react";
-
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import cookies from "react-cookies";
 function News() {
-  return <div>News</div>;
+  const [news, setNews] = useState([]);
+  const [role, setRole] = useState("");
+  const fetchData = async () => {
+    try {
+      const newsData = await axios.get("http://localhost:3002/news");
+      setNews(newsData.data);
+      console.log(newsData.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  useEffect(() => {
+    setRole(cookies.load("role"));
+    fetchData();
+  }, [news]);
+  const deleteNews = async (id) => {
+    const token = cookies.load("token");
+    await axios
+      .delete(`http://localhost:3002/news/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        fetchData();
+      })
+      .catch((e) => console.log(e));
+  };
+  return (
+    <div>
+      {news &&
+        news.map((item, index) => (
+          <li>
+            <p key={index}>
+              {item.title} by {item.user.userName}
+            </p>
+            {role === "admin" && (
+              <>
+                <button onClick={() => deleteNews(item.id)}>delete</button>
+                <button>edit</button>
+              </>
+            )}
+          </li>
+        ))}
+    </div>
+  );
 }
 
 export default News;
